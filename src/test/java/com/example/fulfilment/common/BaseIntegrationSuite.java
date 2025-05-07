af1571dd385c3d983d2d8700830af7ef815e4c67
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2025-05-07 20:19:13 +0200

    Improve BaseIntegrationSuite

--- src/test/java/com/example/fulfilment/common/BaseIntegrationSuite.java
+++ src/test/java/com/example/fulfilment/common/BaseIntegrationSuite.java
@@ -1,21 +1,21 @@
 package com.example.fulfilment.common;
 
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 //This class is used to set up configuration and infrastructure needed for integration testing.
 //All integration test suites should extend this class.
 @SpringBootTest
-@Testcontainers
+@AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // @BeforeAll and @AfterAll methods can be non-static
 public abstract class BaseIntegrationSuite {
 
     private static final String IMAGE = "postgres:12.0";
 
-    @Container
     private static final PostgreSQLContainer<?> POSTGRES =
             new PostgreSQLContainer<>(IMAGE)
                     .withDatabaseName("fulfilment_db")
@@ -23,9 +23,19 @@ public abstract class BaseIntegrationSuite {
                     .withPassword("testpass");
 
     @DynamicPropertySource
-    static void overrideDataSourceProperties(DynamicPropertyRegistry registry) {
+    static void setDataSourceProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url",      POSTGRES::getJdbcUrl);
         registry.add("spring.datasource.username", POSTGRES::getUsername);
         registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+
+    // This code runs once when the BaseIntegrationSuite class is first loaded by JVM. This ensures that
+    // the container is only started once at the start of the test-run lifecycle and reused throughout
+    // the whole test-run. It also ensures that the container is started before anything else, which means
+    // that the container is already running when specific testcases start executing, but also that all the
+    // dynamic properties (values that only become defined once the container is up) are already available
+    // when @DynamicPropertySource methods get called.
+    static {
+        POSTGRES.start(); //this will also register a JVM shutdown hook that calls POSTGRES.stop() which ensures container is cleanly stopped when JVM stops
     }
 }
