418e20f51b469fad2dbc5281a35009019fa96184
matkodipalo <matkodjipalo@gmail.com>
2025-05-09 19:38:00 +0200

    Added a simple validation

--- src/test/java/com/example/fulfilment/controller/ProductControllerIntegrationTest.java
+++ src/test/java/com/example/fulfilment/controller/ProductControllerIntegrationTest.java
@@ -7,7 +7,6 @@ import com.example.fulfilment.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,7 +18,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.BeforeEach;
 
-@AutoConfigureMockMvc
 class ProductControllerIntegrationTest extends BaseIntegrationSuite {
 
     @Autowired
