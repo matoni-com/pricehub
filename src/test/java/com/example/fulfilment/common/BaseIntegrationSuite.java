85697ddf791f61533c6a0fbcd336ee5e7f57a499
matkodipalo <matkodjipalo@gmail.com>
2025-05-09 19:22:28 +0200

    A small fix

--- src/test/java/com/example/fulfilment/common/BaseIntegrationSuite.java
+++ src/test/java/com/example/fulfilment/common/BaseIntegrationSuite.java
@@ -1,5 +1,6 @@
 package com.example.fulfilment.common;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -11,6 +12,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 //All integration test suites should extend this class.
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 public abstract class BaseIntegrationSuite {
 
     private static final String IMAGE = "postgres:12.0";
