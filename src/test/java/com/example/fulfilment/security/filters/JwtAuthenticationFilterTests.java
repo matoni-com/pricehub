f65992d1860aa621729624c3d02679d6334724b5
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2025-05-15 17:42:42 +0200

    Separate integration tests and unit tests.
    Unit tests should be put in test folder and ingration tests shoul be put in integrationTest folder

---
+++ src/test/java/com/example/fulfilment/security/filters/JwtAuthenticationFilterTests.java
@@ -0,0 +1,53 @@
+package com.example.fulfilment.security.filters;
+
+import com.example.fulfilment.security.JwtAuthenticationToken;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtAuthenticationFilterTests {
+
+    // used to mock when object needs to be autowired (injected)
+    @Mock private AuthenticationManager authenticationManager;
+    @InjectMocks private JwtAuthenticationFilter filter;
+
+    @Test
+    @DisplayName("Filter pull token from header, delegates it to auth manager, populates security context and calls chain.doFilter")
+    public void filterAuthenticatesRequestWithBearerToken() throws ServletException, IOException {
+        // used to mock when object does not need to be autowired, usually for complex function arguments
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getHeader("Authorization")).thenReturn("Bearer some_token");
+
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        FilterChain chain = mock(FilterChain.class);
+
+        when(authenticationManager.authenticate(new JwtAuthenticationToken("some_token"))).thenReturn(new JwtAuthenticationToken("some_user", "some_token", null));
+
+        filter.doFilterInternal(request, response, chain);
+
+        // delegates token verification to authenticationManager
+        verify(authenticationManager, times(1)).authenticate(new JwtAuthenticationToken("some_token"));
+
+        // Authentication set in SecurityContext
+        assertEquals(new JwtAuthenticationToken("some_user", "some_token", null), SecurityContextHolder.getContext().getAuthentication());
+
+        // calls chain.doFilter at the end
+        verify(chain, times(1)).doFilter(request, response);
+
+    }
+}
